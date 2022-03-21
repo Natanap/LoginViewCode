b0ddc403483bf7f02178be47385c7b3ecce42cfb
Natanael Alves <natanalvespr@gmail.com>
2022-03-21 14:40:58 -0300

    atualizando

--- LoginViewCode/Home/Cell/UserDetailTableViewCell.swift
+++ LoginViewCode/Home/Cell/UserDetailTableViewCell.swift
@@ -11,25 +11,16 @@ class UserDetailTableViewCell: UITableViewCell {
     
     static let identifier:String = "UserDetailTableViewCell"
     
-    lazy var userImageView:UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.contentMode = .scaleAspectFit
-       return image
-    }()
-    
-    lazy var nameLabel:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .black
+    lazy var userDetailView:UserDetailView = {
+        let view = UserDetailView()
         
-        return label
+        return view
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.addSubview()
-        self.setUpConstraints()
+        self.configConstraintsDescriptionCell()
     }
     
     required init?(coder: NSCoder) {
@@ -37,26 +28,19 @@ class UserDetailTableViewCell: UITableViewCell {
     }
     
     func addSubview(){
-        self.contentView.addSubview(self.userImageView)
-        self.contentView.addSubview(self.nameLabel)
+        self.contentView.addSubview(self.userDetailView)
     }
     
     public func setUpCell(data: DataUser){
-        self.nameLabel.text = data.name
-        self.userImageView.image = UIImage(named: data.nameImage)
+        self.userDetailView.nameLabel.text = data.name
+        self.userDetailView.userImageView.image = UIImage(named: data.nameImage)
         
     }
     
-    private func setUpConstraints(){
-        NSLayoutConstraint.activate([
-        
-            self.userImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            self.userImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.userImageView.heightAnchor.constraint(equalToConstant: 80),
-            self.userImageView.widthAnchor.constraint(equalToConstant: 80),
-            
-            self.nameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            self.nameLabel.leadingAnchor.constraint(equalTo: self.userImageView.trailingAnchor, constant: 20),
-        ])
+    private func configConstraintsDescriptionCell(){
+        self.userDetailView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
+    
 }
